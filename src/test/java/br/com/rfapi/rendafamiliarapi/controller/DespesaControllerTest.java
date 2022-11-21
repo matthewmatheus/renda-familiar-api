9c00d64ff6545dca904cb62890b78ec3018b8e09
Matt <mattmoura.jvm@gmail.com>
2022-11-21 17:49:55 -0300

    Criadas classes DespesaControllerTest e ReceitasControllerTest, não implementadas.

---
+++ src/test/java/br/com/rfapi/rendafamiliarapi/controller/DespesaControllerTest.java
@@ -0,0 +1,28 @@
+package br.com.rfapi.rendafamiliarapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DespesaControllerTest {
+
+    @Test
+    void cadastrarDespesa() {
+    }
+
+    @Test
+    void listarDespesas() {
+    }
+
+    @Test
+    void detalharDespesa() {
+    }
+
+    @Test
+    void atualizarDespesa() {
+    }
+
+    @Test
+    void excluirDespesa() {
+    }
+}
---
+++ src/test/java/br/com/rfapi/rendafamiliarapi/controller/ReceitasControllerTest.java
@@ -0,0 +1,29 @@
+package br.com.rfapi.rendafamiliarapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReceitasControllerTest {
+
+    @Test
+    void cadastrar() {
+    }
+
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void detalharReceita() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void excluirReceita() {
+    }
+}
